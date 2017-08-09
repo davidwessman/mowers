21123b39a419d9cae3d5c387a2841b1f836e1267
David Wessman <david.wesmn@gmail.com>
2017-08-09 10:41:13 +0200

    Mowers: cleanup user views

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  has_many(:mowers)
   validates(:name, :phone, :address, :email, presence: true)
 
   scope(:text_search, lambda do |str|
