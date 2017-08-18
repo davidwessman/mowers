05109ae2f2b4f6ede4247e024110678d4b04155c
David Wessman <david.wesmn@gmail.com>
2017-08-18 10:21:57 +0200

    Redux: setup
    - Constants: add states and types
    - Packs: index for rendering
    - Reducers: combine other reducers
    - Services: setup api-calls
    - Root component for rendering

---
+++ app/javascript/containers/app.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import CustomerTable from 'components/customer_table';
+import Customer from 'components/customer';
+import * as customerActions from 'actions/customers';
+
+const App = ({ customers, actions }) => (
+  <div className="columns">
+    <Customer customer={customers.customer} actions={actions} />
+    <CustomerTable
+      actions={actions}
+      customers={customers.all}
+      state={customers.customer.state}
+    />
+  </div>
+);
+
+App.propTypes = {
+  customers: PropTypes.shape().isRequired,
+  actions: PropTypes.shape().isRequired,
+};
+
+const mapStateToProps = state => ({
+  customers: state.customers,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(customerActions, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
