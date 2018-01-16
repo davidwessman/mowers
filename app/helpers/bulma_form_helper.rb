38458a929504ce7a87bee00c9e8a68d527da547f
David Wessman <david.wesmn@gmail.com>
2018-01-16 07:49:20 +0100

    Rubocop: config and auto-run

--- app/helpers/bulma_form_helper.rb
+++ app/helpers/bulma_form_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module BulmaFormHelper
   def bulma_horizontal(model)
     simple_form_for(model,
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -52,7 +54,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.x.webpacker[:dev_server_host] = "http://127.0.0:8080"
+  config.x.webpacker[:dev_server_host] = 'http://127.0.0:8080'
 
   config.after_initialize do
     Bullet.enable = true
